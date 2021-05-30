df5028e4a7d8f8c4f525fe59cd546b0dce4b391b
ParadoxH88 <paradosz@gmail.com>
2021-05-30 18:07:58 +0300

    2. hazi veglegesitesi fazis, beta
    A 3 heten kiadott 2. hazi veglegesites elotti feltoltese a githubra.

--- 1.hazi/todos.js
+++ 1.hazi/todos.js
@@ -1,28 +1,28 @@
 import { calc } from "./calc.js";
 
 const todo1 = {
-  sorszam: 1,
+  // sorszam: 1,
   name: "Feladat gyakorlás",
   isCompleted: false,
   difficulty: 3,
 };
 
 const todo2 = {
-  sorszam: 2,
+  // sorszam: 2,
   name: "Főzés",
   isCompleted: false,
   difficulty: 4,
 };
 
 const todo3 = {
-  sorszam: 3,
+  // sorszam: 3,
   name: "Mosás",
   isCompleted: false,
   difficulty: 3,
 };
 
 const todo4 = {
-  sorszam: 4,
+  // sorszam: 4,
   name: "Vasalás",
   isCompleted: false,
   difficulty: 2,
@@ -30,10 +30,13 @@ const todo4 = {
 
 const todos = [todo1, todo2, todo3, todo4];
 
+
+let maxDifficulty;
+
 export const initTodos = () => {
   addTodo();
-  feltoltes();
-  szamlalas();
+  initOrderButtons();
+  updateTodos ();
 };
 
 
@@ -45,8 +48,8 @@ const feltoltes = () => {
       container.innerHTML +
       `
     <div class="${todo.isCompleted ? "complete" : "incomplete"}">
-    <input ${todo.isCompleted ? "checked" : ""} type='checkbox'> 
-   ${todo.sorszam}. ${todo.name} - ${todo.difficulty}
+    <input ${todo.isCompleted ? "checked" : ""} type="checkbox"> 
+   ${index +1}. ${todo.name} - ${todo.difficulty}
    <button class="delete">X</button>
     </div>
     `;
@@ -56,16 +59,35 @@ const feltoltes = () => {
 
 const addEventListeners = () => {
   document.querySelectorAll("input[type=checkbox]").forEach((checkbox, index) => {
-      checkbox.addEventListener("click", () => {
+      checkbox.addEventListener("change", () => {
         todos[index].isCompleted = checkbox.checked;
         // szamlalas(); proba
-        feltoltes();
+        updateTodos();
       });
     });
+
+  document.querySelectorAll(".delete").forEach((button, index) => {
+    button.addEventListener("click", () => {
+      todos.splice(index, 1);
+      updateTodos();
+    });
+  });
 };
 
 const szamlalas = () => {
   document.querySelector("#teljesitesiArany").textContent = `${Math.round(calc(todos) * 100)}%`;
+};
+
+const initOrderButtons = () => {
+  document.querySelector('#novekvo').addEventListener('click', () => {
+      todos.sort((t1, t2) => t1.difficulty < t2.difficulty ? -1 : (t1.difficulty === t2.difficulty ? 0 : 1));
+      updateTodos();
+  });
+
+  document.querySelector('#csokkeno').addEventListener('click', () => {
+      todos.sort((t1, t2) => t1.difficulty < t2.difficulty ? 1 : (t1.difficulty === t2.difficulty ? 0 : -1));
+      updateTodos();
+  });
 };
 
 
@@ -76,11 +98,29 @@ const addTodo = () => {
     document.querySelector('#hozzaadtext').value = '';
     document.querySelector('#hozzaaddif').value = '';
     const todon = {
-      sorszam: todos{index},
+      // sorszam: todos[index],
       name: text,
       difficulty: dif,
     } 
     todos.push(todon);
-    feltoltes();
-  })
-}
+    updateTodos();
+  });
+};
+
+const updateMaxDifficulty = () => {
+  maxDifficulty = Math.max(...todos.map(t => t.difficulty));
+  document.querySelector('#max').textContent = maxDifficulty;
+};
+
+const updateHardestTodo = () => {
+  document.querySelector('#hardest').textContent = todos.find(t => t.difficulty === maxDifficulty).name;
+};
+
+const updateTodos = () => {
+  feltoltes();
+  addEventListeners ();
+  szamlalas();
+  updateMaxDifficulty();
+  updateHardestTodo();
+ 
+};
